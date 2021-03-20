db8913335ba1b0e8682d8e25cfab84e30858c2df
anna7br <anna.siebenbrunner@gmail.com>
2021-03-20 21:31:40 +0100

    String Interpolation

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,2 +1,4 @@
-let favoriteAnimal = 'moose';
-console.log('My favorite animal: ' + favoriteAnimal);
+let myName = 'Anna';
+let myCity = 'Innsbrooklyn';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)

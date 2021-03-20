5be236f366b82a9d620281190d838bc1ebc72b97
anna7br <anna.siebenbrunner@gmail.com>
2021-03-20 21:34:03 +0100

    typeof operator

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,4 +1,4 @@
-let myName = 'Anna';
-let myCity = 'Innsbrooklyn';
-
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1
+console.log(typeof newVariable);

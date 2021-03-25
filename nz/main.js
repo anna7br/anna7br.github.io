8226b8af9c81bf9275dce83764a181227f1cad93
anna7br <anna.siebenbrunner@gmail.com>
2021-03-25 16:34:43 +0100

    stop objekt erzeugt und verwendet

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,21 @@
+let stop = {
+    nr: 6,
+    name: "Moeraki Boulders",
+    lat: -45.345275,
+    lng: 170.826061,
+    user: "anna7br",
+    wikipedia: "https://en.wikipedia.org/wiki/Moeraki_Boulders",
+};
+
 const map = L.map("map", {
-    center: [-45.345275, 170.826061],
+    center: [stop.lat, stop.lng],
     zoom: [13],
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
-let mrk = L.marker([-45.345275, 170.826061]).addTo(map);
+let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
 mrk.bindPopup("Moeraki Boulders").openPopup();
 
-console.log(document.querySelector("#map"));
+//console.log(document.querySelector("#map"));

bea2bbabf31e07d5da579a9485286ad637c75448
anna7br <anna.siebenbrunner@gmail.com>
2021-03-25 17:58:13 +0100

    Pull down befüllen

--- nz/main.js
+++ nz/main.js
@@ -15,9 +15,16 @@ const map = L.map("map", {
     ]
 });
 
-console.log(ROUTE);
+
+let nav = document.querySelector('#navigation');
+console.log(nav);
+
+//console.log(ROUTE);
 for (let entry of ROUTE) {
   // console.log(entry);
+  nav.innerHTML += `
+    <option value="${entry.user}r">Stop ${entry.nr}: ${entry.name}</option>
+  `;
   let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
   mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
 <p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
@@ -30,3 +37,5 @@ for (let entry of ROUTE) {
 }
 
 console.log(document.querySelector("#map"));
+
+//            <option value="anna7br">Moeraki_Boulders</option>

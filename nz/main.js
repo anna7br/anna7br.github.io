a2125a96ce54076604e2b5b2f23003037ca7db7b
anna7br <anna.siebenbrunner@gmail.com>
2021-03-25 16:13:35 +0100

    Karte scheint auf zentriert

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,9 @@
-const map = L.map("map");
+const map = L.map("map", {
+    center: [-45.345275, 170.826061],
+    zoom: [13],
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+//
 console.log(document.querySelector("#map"));

85bcecb296996aa23637d9e2ab319696a68e6e47
anna7br <anna.siebenbrunner@gmail.com>
2021-03-25 17:03:06 +0100

    popup mit templatesyntax verbessert

--- nz/main.js
+++ nz/main.js
@@ -17,7 +17,7 @@ const map = L.map("map", {
 
 let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
 mrk.bindPopup(`<h4>Stop ${stop.nr}: ${stop.name}</h4>
-<p><a href="${stop.wikipedia}" Read about stop in Wikipedia</a></p>
+<p><i class="fas fa-external-link-alt mr-3"></i><a href="${stop.wikipedia}">Read about stop in Wikipedia</a></p>
 `).openPopup();
 
 

f38bb991a3fd7400f27e2442c2b3b47b4039e01c
anna7br <anna.siebenbrunner@gmail.com>
2021-03-28 19:41:05 +0200

    Axamer Lizum eingesetzt

--- pow/main.js
+++ pow/main.js
@@ -1,10 +1,9 @@
 let stop = {
-    nr: 6,
-    name: "Moeraki Boulders",
-    lat: -45.345275,
-    lng: 170.826061,
-    user: "anna7br",
-    wikipedia: "https://en.wikipedia.org/wiki/Moeraki_Boulders",
+    name: "Axamer Lizum",
+    lat: 47.196505613395935,
+    lng: 11.302607045102148,
+    bundesland: "Tirol",
+    more: "https://en.wikipedia.org/wiki/Moeraki_Boulders",
 };
 
 const map = L.map("map", {
@@ -27,25 +26,25 @@ ROUTE.sort((stop1, stop2) => {
 
 
 for (let entry of ROUTE) {
-  // console.log(entry);
-  nav.innerHTML += `
+    // console.log(entry);
+    nav.innerHTML += `
     <option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>
   `;
-  let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
-  mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
+    let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+    mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
 <p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
 `);
-  if (entry.nr == 6) {
-    map.setView([entry.lat, entry.lng], 13);
-    mrk.openPopup();
-  }
+    if (entry.nr == 6) {
+        map.setView([entry.lat, entry.lng], 13);
+        mrk.openPopup();
+    }
 
 }
 nav.options.selectedIndex = 6 - 1;
 nav.onchange = (evt) => {
     let selected = evt.target.selectedIndex;
     let options = evt.target.options;
-    let username =options[selected].value;
+    let username = options[selected].value;
     let link = `https://${username}.github.io/nz/index.html`;
     console.log(username, link);
 

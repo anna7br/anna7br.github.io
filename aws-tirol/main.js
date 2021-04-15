4e89eedf981ccb7fdba3d8ec95f1b035dcfe7a80
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-04-15 17:58:56 +0200

    Korrekturen

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -24,37 +24,56 @@ let layerControl = L.control.layers({
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
 awsLayer.addTo(map);
+let snowLayer = L.featureGroup();
+layerControl.addOverlay(snowLayer, "Schneehöhen");
+snowLayer.addTo(map);
 
 let awsUrl ='https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
-fetch(awsUrl)
+etch(awsUrl)
     .then(response => response.json())
     .then(json => {
-        //console.log('Daten konvertiert: ', json);
+        console.log('Daten konvertiert: ', json);
         for (station of json.features) {
-            //console.log('Station: ', station);
+            // console.log('Station: ', station);
             let marker = L.marker([
                 station.geometry.coordinates[1],
                 station.geometry.coordinates[0]
             ]);
-
             let formattedDate = new Date(station.properties.date);
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul>
-                <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
-                <li>Datum: ${formattedDate.toLocaleString("de")}</li>
-                <li>Temperatur: ${station.properties.LT || '?'} °C</li>
-                <li>Luftfeuchtigkeit: ${station.properties.RH || '?'} %</li>
-                <li>Schneehöhe: ${station.properties.HS || '?'} m</li>
-                <li>Windgeschwindigkeit: ${station.properties.WG|| '?'} m/s</li>
-                <li>Windrichtung: ${station.properties.WR || '?'}</li>
+              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+              <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
+              <li>Temperatur: ${station.properties.LT} C</li>
+              <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
+              <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
+              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
             </ul>
-            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/§{station.properties.plot}.png">Grafik</a>
+            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let highlightClass = '';
+                if (station.properties.HS > 100) {
+                    highlightClass = 'snow-100';
+                }
+                if (station.properties.HS > 200) {
+                    highlightClass = 'snow-200';
+                }
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label ${highlightClass}">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(snowLayer);
+
         }
         // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
     });
-

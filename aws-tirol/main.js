d1d30c875df566526dd7d24f1ffe2f4c7ddd94f9
Anna Siebenbrunner <72545186+anna7br@users.noreply.github.com>
2021-04-15 16:02:52 +0200

    Leaflet einbinden

---
+++ aws-tirol/main.js
@@ -0,0 +1,18 @@
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
+
+
+
+let map = L.map("map", {
+    center: [47, 11],
+    zoom: 9,
+    layers: [
+        basemapGray
+    ]
+});
+
+
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthophoto": L.tileLayer.provider('BasemapAT.ortophoto')
+}).addTo(map);
+
